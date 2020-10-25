b830bdc18124ab864c997f664415bcbb96d2d526
nathangrigne <nathan.grigne@hetic.net>
2020-10-25 11:29:21 +0100

    :pencil2: Edit example Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
+    <!-- Call component -->
     <Test />
   </div>
 </template>
 
 <script>
+
+// Import Component PS : Caps is necessary on the first letter of tour component
 import Test from "@/components/Test.vue";
 
 export default {
+  // Export Vue
   name: "Home",
   components: {
     Test
