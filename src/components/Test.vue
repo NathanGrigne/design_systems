115e8b00ab6f9453753470979384f2405ca41531
nathangrigne <nathan.grigne@hetic.net>
2020-10-25 11:28:57 +0100

    :pencil2: Edit example component

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="hello">
-    <h1>NIQUE TA MERE JESS</h1>
+  <!-- HTML CODE -->
+  <div class="container">
+    <h2 class="title">My Input</h2>
+    <input type="text" class="input" placeholder="Text">
   </div>
 </template>
 
 <script>
+// JS CODE
+const input = document.querySelector('.input')
+
+// Export Component
 export default {
+  // Give name
   name: "Test"
 };
 </script>
 
 <style lang="scss" scoped>
+
+// CSS CODE
+
 h1 {
   font-family: Garamond_regular;
   color: --error-100-color;
 }
+
 </style>
