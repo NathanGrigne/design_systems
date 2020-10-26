70863c33139bf2a83c6d3c458b289b6574572233
nathangrigne <nathan.grigne@hetic.net>
2020-10-26 14:31:31 +0100

    update view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <!-- Call component -->
     <Test />
     <Buttons />
+    <Checkbox />
   </div>
 </template>
 
@@ -10,14 +11,16 @@
 
 // Import Component PS : Caps is necessary on the first letter of tour component
 import Test from "@/components/Test.vue";
-import Buttons from "@/components/Buttons.vue"
+import Buttons from "@/components/Buttons.vue";
+import Checkbox from "@/components/Checkbox.vue"
 
 export default {
   // Export Vue
   name: "Home",
   components: {
     Test,
-    Buttons
+    Buttons,
+    Checkbox
   }
 };
 </script>
