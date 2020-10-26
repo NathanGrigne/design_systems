c35a95c7ffbb7c6ab29ec8e2c4a369a9870b8de0
nathangrigne <nathan.grigne@hetic.net>
2020-10-26 14:38:29 +0100

    update component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,6 @@
 <script>
 
 // Import Component PS : Caps is necessary on the first letter of tour component
-import Test from "@/components/Test.vue";
 import Buttons from "@/components/Buttons.vue";
 import Checkbox from "@/components/Checkbox.vue"
 
@@ -18,7 +17,6 @@ export default {
   // Export Vue
   name: "Home",
   components: {
-    Test,
     Buttons,
     Checkbox
   }
