a137c95b607aa119a041d057d7eb2159561f3fa4
nathangrigne <nathan.grigne@hetic.net>
2020-10-26 14:54:59 +0100

    seperate Buttons.vue into 2 component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- HTML CODE -->
+  <div class="container-buttons">
+      <div class="primary-button">
+          <p>Primary</p>
+      </div>
+  </div>
+</template>
+
+<script>
+// Export Component
+export default {
+  // Give name
+  name: "PrimaryButton"
+};
+</script>
+
+<style lang="scss" scoped>
+
+// CSS CODE
+
+.container-buttons{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+}
+
+.primary-button{
+    width: 182px;
+    height: 48px;
+    border-radius: 8px;
+    background-color: var(--primary-70-color);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+}
+
+.primary-button:hover{
+    background-color: var(--primary-90-color)
+}
+
+.primary-button:active{
+    background-color: var(--primary-100-color)
+}
+
+p{
+  font-family: Typold-medium;
+  font-size: 16px;
+  color: #FFFFFF;
+  margin: 0;
+  letter-spacing: 1px;
+}
+
+</style>
---
+++ src/components/SecondaryButton.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="container-buttons">
+      <div class="secondary-button">
+          <p>Secondary</p>
+      </div>
+  </div>
+</template>
+
+<script>
+// Export Component
+export default {
+  // Give name
+  name: "SecondaryButton"
+};
+</script>
+
+<style lang="scss" scoped>
+
+// CSS CODE
+
+.container-buttons{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+}
+
+.secondary-button{
+    width: 182px;
+    height: 48px;
+    border-radius: 8px;
+    background-color: var(--secondary-70-color);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+}
+
+.secondary-button:hover{
+    background-color: var(--secondary-90-color)
+}
+
+.secondary-button:active{
+    background-color: var(--secondary-100-color)
+}
+
+p{
+  font-family: Typold-medium;
+  font-size: 16px;
+  color: #FFFFFF;
+  margin: 0;
+  letter-spacing: 1px;
+}
+
+</style>
